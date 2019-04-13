7eaa00cdd2f6c60c477688923b8ebbda6b9fa85e
Shivam Agrawal <shivamagrawal@wireless-10-104-33-22.umd.edu>
2019-04-13 19:55:46 -0400

    working on image search feature

--- server/app.py
+++ server/app.py
@@ -1,4 +1,8 @@
+import os
+import glob
+
 from flask import Flask, request, send_from_directory
+from google_images_download import google_images_download
 
 app = Flask(__name__, static_url_path='/EZ-CV/client')
 
@@ -8,12 +12,31 @@ def index(): return send_from_directory('../client/', 'index.html')
 @app.route('/style.css')
 def style(): return send_from_directory('../client/', 'style.css')
 
+# Static Image Serving
+
+img_dir = '/home/sagrawa2/EZ-CV/images/'
+
+@app.route('/img/<path:path>')
+def send_image(path):
+	return send_from_directory('/../images', path)
+
 # Search for images
 @app.route('/image_search', methods=['POST'])
 def image_search():
-	print('image search query:')
-	print(request.get_json())
-	print(request.get_json()['query'])
+
+	# Delete all images in the image directory
+	old_imgs = glob.glob(img_dir + "*")
+	for f in files: os.remove(f)
+
+	# Download the new images
+	query = request.get_json()['query']
+	response = google_images_download.googleimagesdownload()
+	absolute_image_paths = response.download({
+		'keywords': query,
+		'limit': 20,
+		'output_directory': img_dir
+	})
+
 	return 'all good'
 
 # Train the model
