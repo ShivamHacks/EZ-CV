08c195aa650bb1950eb556a367013b97cf081aec
Shivam Agrawal <shivamagrawal@Shivams-MacBook-Air.local>
2019-04-14 08:57:36 -0400

    final few updates before hackathon submission

--- server/app.py
+++ server/app.py
@@ -56,7 +56,7 @@ def image_search():
 @app.route('/train_model', methods=['POST'])
 def train_model():
 	annotations = request.get_json()
-	with open('../keras_frcnn/kitti_simple_label.txt', 'a') as f:
+	with open('../keras_frcnn/kitti_simple_label.txt', 'w') as f:
 		for img_id, bbox, in annotations.items():
 			img_path = '/home/sagrawa2/EZ-CV/images/' + img_id.split('http://35.237.13.210:8080/img/')[1]
 			f.write(img_path + ',' + ','.join(map(str, bbox)) + ',bird\n')
